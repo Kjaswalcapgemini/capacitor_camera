9b11537fb8f4d96a3b237fddde0a03483b42b379
karan62kj <48881570+karan62kj@users.noreply.github.com>
2024-04-25 17:57:42 +0530

    initial commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,87 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+  <div class="cameraPreview">
+    <video id="cameraPreview" ref="cameraPreview" playsinline v-show="!imageClicked"></video>
+    <img id="image" v-show="imageClicked" :src="imageSrc" ref="capturedImgRef" alt="Captured Image" />
+  </div>
+  <div class="clickButton" @click="takePicture">
+  Take Picture</div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
+  mounted() {
+    this.startCamera();
+  },
+  data() {
+    return {
+      imageSrc:"",
+      imageClicked: false
+    }
+  },
   props: {
     msg: String
-  }
-}
+  },
+  methods:{
+    async startCamera(){
+      const video = this.$refs.cameraPreview;
+      this.mediaStream = await navigator.mediaDevices.getUserMedia({
+        video:{
+          width:{ideal:1024},
+          facingMode:"environment",
+          autoFocus:"true",
+          aspectRatio:{ideal:4/5}
+        }
+      });
+      video.srcObject = this.mediaStream;
+      video.play();
+      this.cameraPreview = true;
+    },
+    async closeCameraPreview(){
+      return new Promise((resolve, reject) => {
+          if(this.mediaStream){
+            const tracks = this.mediaStream.getTracks();
+            const promises = [];
+            tracks.forEach(track => {
+              promises.push(new Promise((trackResolve,trackReject) => {
+                try{
+                  track.stop();
+                  this.mediaStream.removeTrack(track);
+                  trackResolve();
+                }catch(e){
+                  trackReject(e);
+                }
+              }));
+            });
+      Promise.all(promises).then(() => {
+        this.mediaStream = null;
+        this.cameraPreview = false;
+        resolve();
+      }).catch(reject);
+     }else{
+      resolve();
+      }
+    });
+    },
+    async takePicture(){
+      const video = this.$refs.cameraPreview;
+      const canvas = document.createElement("canvas");
+      const ctx = canvas.getContext("2d");
+      canvas.width = video.videoWidth;
+      canvas.height = video.videoHeight;
+      ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+      const base64PictureData = canvas.toDataURL("image/png");
+      await this.closeCameraPreview();
+      this.imageSrc = base64PictureData;
+      this.imageClicked = true;
+    }
+},
+};
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+<style>
+#cameraPreview{
+  width: calc(100vw -30px);
 }
 </style>
